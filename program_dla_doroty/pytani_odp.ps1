1360c92c5d32530706680a7bd956da7e2041444e
mlodynofear <mlodynofear>
2018-01-24 22:16:26 +0100

    kocham dorote

---
+++ program_dla_doroty/pytani_odp.ps1
@@ -0,0 +1,30 @@
+﻿Write-Host -BackgroundColor DarkYellow "Przemek, nie ma teraz pracy, i jest to spowodowane jego chorobą."
+Write-Host -BackgroundColor DarkYellow "Jest takze glupi iz to zrobił, ale jak mowie jest to choroba!" 
+           
+Write-Host -BackgroundColor DarkRed "Odpowiedz tak lub nie"
+
+$pytanie1 = Read-Host -Prompt  'Czy nadal kochasz Przemka?'
+$pytanie2 = Read-Host -Prompt 'Czy chcesz być z nim?'
+$pytanie3 = Read-Host -Prompt 'Czy widzisz ze pracuje nad sobą?'
+
+
+$serce = Get-Content C:\Users\Elmo\Desktop\program_dla_doroty\serce.txt
+$swieta = Get-Content C:\Users\Elmo\Desktop\program_dla_doroty\swieta.txt 
+
+IF ($pytanie1 -eq 'tak' -AND $pytanie2 -eq'tak' -AND $pytanie3 -eq'tak')
+   {
+
+   $serce
+
+   }
+
+ELSE
+
+   {
+
+   $swieta 
+   
+   }
+
+
+Read-Host -Prompt "Przycisnij ENTER aby wyjść"
---
+++ program_dla_doroty_2/program_dorota.ps1
@@ -0,0 +1,47 @@
+﻿Write-Host "Witaj Dorota!!" -ForegroundColor Red
+""
+Write-Host "Proszę odpowiadaj, tak lub nie, dziekuję." -ForegroundColor Red
+""
+Write-Host "Pobaw się odpowiedziami tak lub nie aby zobaczyć wszystkie 4 obrazki!" -BackgroundColor DarkGray -ForegroundColor Blue
+""
+Write-Host "Miłej ZABAWY!!!" -BackgroundColor DarkGray -ForegroundColor Blue
+""
+$question_1 = Read-Host "Zadawałaś Sobie pytanie czy Przemkowi czasami nie jest ciężko??"
+""
+$question_2 = Read-Host "Czy wiesz że cągle szuka pracy??"
+""
+$question_3 = Read-Host "Czy wiesz ze zrozumiał swój błąd??"
+""
+$question_4 = Read-Host "Czy wiesz że bardzo Cię kocha??"
+""
+$xmass = Get-Content C:\Users\Elmo\Desktop\program_dla_doroty_2\mikolaj.txt
+
+
+if($question_1 -eq 'tak' -and $question_2 -eq 'tak' -and $question_3 -eq 'tak' -and $question_4 -eq 'tak')
+  {
+  Get-Content C:\Users\Elmo\Desktop\program_dla_doroty_2\milosc.txt
+  }
+
+elseif($question_1 -eq 'tak' -and $question_2 -eq 'tak' -and $question_3 -eq 'nie' -and $question_4 -eq 'tak')
+  {
+  Get-Content C:\Users\Elmo\Desktop\program_dla_doroty_2\nie_wie.txt
+  }
+  
+elseif($question_1 -eq 'tak' -and $question_2 -eq 'nie' -and $question_3 -eq 'nie' -and $question_4 -eq 'tak')
+  {
+  Get-Content C:\Users\Elmo\Desktop\program_dla_doroty_2\wszysztko_nie.txt
+  }
+
+else
+  {
+  Get-Content C:\Users\Elmo\Desktop\program_dla_doroty_2\koniec.txt
+  }
+
+""
+""
+""
+""
+""
+  $xmass
+""
+  Read-Host "Wcisnij ENTER aby kontynuować..."
